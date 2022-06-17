8d7a8783c05624cc8e821a04238884c1875a36d5
Lucas <dbui5430@gmail.com>
2022-06-17 13:41:40 +0700

    lesson_2

---
+++ rustlesson2_fixprg/target/debug/deps/rustlesson2_fixprg.d
@@ -0,0 +1,5 @@
+C:\Users\ADMIN\rust\rustlesson2_fixprg\target\debug\deps\rustlesson2_fixprg.exe: src\main.rs
+
+C:\Users\ADMIN\rust\rustlesson2_fixprg\target\debug\deps\rustlesson2_fixprg.d: src\main.rs
+
+src\main.rs:
---
+++ rustlesson2_fixprg/target/debug/rustlesson2_fixprg.d
@@ -0,0 +1,1 @@
+C:\Users\ADMIN\rust\rustlesson2_fixprg\target\debug\rustlesson2_fixprg.exe: C:\Users\ADMIN\rust\rustlesson2_fixprg\src\main.rs
